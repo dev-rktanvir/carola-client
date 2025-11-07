09b9d909bfaba012ee2e04bd6e208fecf85d9af5
rk-tanvir790 <ktanvir790@gmail.com>
2025-11-07 16:03:03 +0600

    setup project and insatll tailwind css, react router, google font

---
+++ src/Pages/StaticPages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='bg-primary'>
+            <h1>I am tanvir</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla natus, iusto ad esse facere vero ea qui nostrum laboriosam placeat vitae, ipsa ratione laudantium quod praesentium porro amet magni. Veniam necessitatibus quibusdam, eos labore reprehenderit accusantium numquam nesciunt, fugit ea corporis atque quod autem esse, incidunt delectus praesentium quia dicta.</p>
+        </div>
+    );
+};
+
+export default Home;
